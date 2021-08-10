52b5dea8651ee4e5f98e1f397420222f18685c38
William Xu <yihangxu001@hotmail.com>
2021-08-10 10:25:51 -0700

    Add voi remote scripts

---
+++ R/voi.remote.R
@@ -0,0 +1,36 @@
+# set.seed(1) 
+# nsam <- 10000
+# inputs <- data.frame(p1 = rnorm(nsam, 1, 1), 
+#                      p2 = rnorm(nsam, 0, 2))
+# outputs_nb <- data.frame(t1 = 0, 
+#                          t2 = inputs$p1 - inputs$p2)
+
+evpi.remote <- function(outputs, nsim) {
+  model_input <- list(outputs=outputs, nsim=nsim, method='evpi')
+  res <- model_run(model_name='voiPrism', model_input, api_key='', server='')
+  return(res)
+}
+
+evppi.remote <- function(outputs, inputs, pars, se, B, nsim, verbos, method) {
+  model_input <- list(outputs=outputs, inputs=inputs, pars=pars, se=se, B=B, nsim=nsim, vernos=verbos, method='evppi')
+  res <- model_run(model_name='voiPrism', model_input, api_key='', server='')
+  return(res)
+}
+
+evppi_mc.remote <- function(model_fn, par_fn, pars, nouter, ninner, wtp, mfargs, verbos, method) {
+  model_input <- list(model_fn=model_fn, par_fn=par_fn, pars=pars, nouter=nouter, ninner=ninner, wtp=wtp, mfargs=mfargs, verbos=verbos, method='evppi_mc')
+  res <- model_run(model_name='voiPrism', model_input, api_key='', server='')
+  return(res)
+}
+
+evsi.remote <- function(outputs, inputs, study, datagen_fn, pars, n, likelihood, analysis_model, analysis_options, decision_model, Q, npreg_method, nsim, verbos, method, ...) {
+  model_input <- list(outputs=outputs, inputs=inputs, study=study, datagen_fn=datagen_fn, pars=pars, n=n, likelihood=likelihood, analysis_model=analysis_model, analysis_options=analysis_options, decision_model=decision_model, Q=Q, npreg_method=npreg_method, nsim, verbos, method='evsi')
+  res <- model_run(model_name='voiPrism', model_input, api_key='', server='')
+  return(res)
+}
+
+evsivar.remote <- function(outputs, inputs, study, datagen_fn, pars, n, likelihood, analysis_model, analysis_options, decision_model, Q, npreg_method, nsim, verbos, method, ...) {
+  model_input <- list(outputs=outputs, inputs=inputs, study=study, datagen_fn=datagen_fn, pars=pars, n=n, likelihood=likelihood, analysis_model=analysis_model, analysis_options=analysis_options, decision_model=decision_model, Q=Q, npreg_method=npreg_method, nsim, verbos, method='evsivar')
+  res <- model_run(model_name='voiPrism', model_input, api_key='', server='')
+  return(res)
+}
